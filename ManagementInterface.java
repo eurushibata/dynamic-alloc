e4619c42afd31720b7bcee43398ab5ee1faeb282
eurushibata <urushibata.emerson@gmail.com>
2013-05-15 02:16:41 -0300

    first commit

---
+++ ManagementInterface.java
@@ -0,0 +1,20 @@
+public interface ManagementInterface {
+
+	// aloca um bloco de memoria para um processo 
+	public boolean allocateMemoryBlock(int processId, int size) throws MemoryOverflow;
+
+	// libera um bloco de memoria ocupado por um processo
+	public boolean freeMemoryBlock(int processId);
+	
+	// libera todos os blocos de memoria ocupados
+	public void freeAll();
+
+ 	// redistribui o conteudo da memoria de modo a criar um grande e unico bloco de memoria livre
+	public void compactMemory();
+	
+	// traduz um endereco logico de um processo para um endereco fisico
+	public int getPhysicalAddress(int processId, int logicalAddress) throws InvalidAddress;
+
+	// processa um arquivo texto contendo um conjunto de comandos
+	public boolean processCommandFile(String fileName);
+}
---
+++ NotificationInterface.java
@@ -0,0 +1,5 @@
+public interface NotificationInterface {
+	
+	// notifica a alocacao de quadros aos diferentes processos carregados na memoria
+	public void displayMemoryMap(int[][] memoryBlocks);
+}
