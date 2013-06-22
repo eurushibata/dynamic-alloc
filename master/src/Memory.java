f06f348b057af4deb2e0df0f3a895f98c6781537
eurushibata <urushibata.emerson@gmail.com>
2013-06-22 01:40:44 -0300

    modificações nas excessões e implementação do método compactMemory

--- master/src/Memory.java
+++ master/src/Memory.java
@@ -1,4 +1,7 @@
 
+import exceptions.MemoryOverflow;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import trabso.ContiguousAllocationManager;
 
 /*
@@ -19,14 +22,20 @@ public class Memory {
     public static void main(String[] args) {
         // TODO code application logic here
         ContiguousAllocationManager a = new ContiguousAllocationManager(6);
-//        System.out.println(a.dynamicMemory.get(0).getProcess());
-        a.allocateMemoryBlock(1, 3);
-        a.allocateMemoryBlock(2, 2);
-        a.allocateMemoryBlock(3, 1);
-        a.freeAll();
+        try {
+            //        System.out.println(a.dynamicMemory.get(0).getProcess());
+                    a.allocateMemoryBlock(1, 3);
+                    a.allocateMemoryBlock(2, 2);
+                    a.allocateMemoryBlock(3, 1);
+        } catch (MemoryOverflow ex) {
+            Logger.getLogger(Memory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+//        a.freeAll();
         a.freeMemoryBlock(1);
-        a.freeMemoryBlock(2);
+//        a.freeMemoryBlock(2);
         a.freeMemoryBlock(3);
+        a.compactMemory();
         for(int i=0; i<a.dynamicMemory.size(); i++) {
             System.out.println(a.dynamicMemory.get(i).getProcess() + "/" + a.dynamicMemory.get(i).getBase()+"/"+a.dynamicMemory.get(i).getSize());
         }
